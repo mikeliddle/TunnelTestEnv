f320c3912dfafb09d763766e2ded0a161a39faac
Mike Liddle <maliddle96@gmail.com>
2023-06-22 14:26:38 -0600

    added provisioning script

--- scripts/TunnelProfiles.ps1
+++ scripts/TunnelProfiles.ps1
@@ -31,6 +31,80 @@ Function Login-Graph {
 }
 #endregion Graph Functions
 
+#region App Registration Setup
+Function New-ServicePrincipal {
+    param(
+        [String]$AADEnvironment
+    )
+
+    Import-Module -Name AzureAD
+
+    if ($AADEnvironment -ieq "onedf" -or $AADEnvironment -ieq "df" -or $AADEnvironment -ieq "internal") {
+        try {
+            Connect-AzureAD -AzureEnvironmentName AzurePPE
+        } catch [Exception] {
+            Write-Error "Error occured connecting to AAD"
+            Write-Host $_.Exception.GetType().FullName, $_.Exception.Message
+            Write-Host "Failed to provision the Service Principal" -ForegroundColor Red
+            exit 1
+        }
+    } elseif ($AADEnvironment -ieq "germany" -or $AADEnvironment -ieq "blackforest" ) {
+        try {
+            Connect-AzureAD -AzureEnvironmentName AzureGermanyCloud
+        } catch [Exception] {
+            Write-Error "Error occured connecting to AAD"
+            Write-Host $_.Exception.GetType().FullName, $_.Exception.Message
+            Write-Host "Failed to provision the Service Principal" -ForegroundColor Red
+            exit 1
+        }
+    } elseif ($AADEnvironment -ieq "government" -or $AADEnvironment -ieq "fairfax" ) {
+        try {
+            Connect-AzureAD -AzureEnvironmentName AzureUSGovernment
+        } catch [Exception] {
+            Write-Error "Error occured connecting to AAD"
+            Write-Host $_.Exception.GetType().FullName, $_.Exception.Message
+            Write-Host "Failed to provision the Service Principal" -ForegroundColor Red
+            exit 1
+        }
+    } elseif ($AADEnvironment -ieq "china" -or $AADEnvironment -ieq "mooncake" ) {
+        try {
+            Connect-AzureAD -AzureEnvironmentName AzureChinaCloud
+        } catch [Exception] {
+            Write-Error "Error occured connecting to AAD"
+            Write-Host $_.Exception.GetType().FullName, $_.Exception.Message
+            Write-Host "Failed to provision the Service Principal" -ForegroundColor Red
+            exit 1
+        }
+    } else  {
+        try {
+            Connect-AzureAD
+        } catch [Exception] {
+            Write-Error "Error occured connecting to AAD"
+            Write-Host $_.Exception.GetType().FullName, $_.Exception.Message
+            Write-Host "Failed to provision the Service Principal" -ForegroundColor Red
+            exit 1
+        }
+    }
+
+    try {
+        $appId = "3678c9e9-9681-447a-974d-d19f668fcd88"
+        
+        New-AzureADServicePrincipal -AppId $appId
+        $result = Get-AzureADServicePrincipal -Filter "AppID eq '$appId'"
+    
+        Write-Host $result
+    
+        $displayName = $result.AppDisplayName
+        Write-Host "Successfully provisioned the Service Principal for $displayName" -ForegroundColor Green
+    } catch [Exception] {
+        Write-Error "Error provisioning Service Principal"
+        Write-Host $_.Exception.GetType().FullName, $_.Exception.Message
+        Write-Host "Failed to provision the Service Principal" -ForegroundColor Red
+        exit 1
+    }
+}
+#endregion
+
 #region Tunnel Server Profiles
 Function New-TunnelConfiguration {
     param(
