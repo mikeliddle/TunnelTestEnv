6bcb60bec8ec320d27b48a627aae95f9ee7b029c
Mike Liddle <maliddle96@gmail.com>
2023-07-18 09:43:27 -0600

    updates to ADFS

--- scripts/SetupADDC.ps1
+++ scripts/SetupADDC.ps1
@@ -5,7 +5,9 @@ $script:WindowsVmSize = "Standard_DS1_v2"
 Function Initialize-ADFSVariables {
     $script:VmName = $VmName.ToLower()
     $script:Account = (az account show | ConvertFrom-Json)
-    $script:Subscription = $Account.id
+    $script:Username = "AdminUser"
+    $script:SubscriptionId = $Account.id
+    $script:location = "westus"
 
     if ($Email -eq "") {
         Write-Header "Email not provided. Detecting email..."
@@ -15,38 +17,43 @@ Function Initialize-ADFSVariables {
 
     $script:ResourceGroup = "$VmName-group"
     $script:GraphContext = Get-MgContext
+
+    $script:WindowsServerImage = "Win2022AzureEditionCore"
 }
 
 Function New-ADFSEnvironment {
+    param(
+        [string] $VmName,
+        [string] $DomainName,
+        [string] $ResourceGroup,
+        [string] $location
+    )
     Write-Header "Creating ADFS Environment"
 
-    Test-Prerequisites
-    Login
+    Login-Azure -SubscriptionId $SubscriptionId -VmTenantCredential $VmTenantCredential
+    
     Initialize-ADFSVariables
-    New-ResourceGroup
+    New-ResourceGroup -resourceGroup $ResourceGroup -location $location | Out-Null
 
     New-ADDCVM
-
-    # Temporarily undo all the work done to create
-    # Remove-ResourceGroup
 }
 
 Function New-ADDCVM {
     $AdminPassword = New-RandomPassword
-    $length = if($VmName.Length -gt 12) { 12 } Else { $VmName.Length }
+    $length = if($VmName.Length -gt 12) { 12 } else { $VmName.Length }
     $winName=$VmName.Substring(0,$length) + "-dc"
     
     Write-Header "Creating VM '$winName'..."
 
     $windowsVmData = az vm create --location $location --resource-group $resourceGroup --name $winName --image $WindowsServerImage --size $WindowsVmSize --public-ip-address-dns-name "$VmName-dc" --admin-username $Username --admin-password $AdminPassword --only-show-errors | ConvertFrom-Json
 `
+    $domainLength = if("$DomainName".Split('.')[0].Length -gt 15) { 15 } Else { "$DomainName".Split('.')[0].Length }
     # Install AD DS role on the first VM and promote it to a domain controller
     az vm run-command invoke --command-id RunPowerShellScript --resource-group $resourceGroup --name $winName --scripts @"
         Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
         Import-Module ADDSDeployment
-        $length = if($DomainName.Split('.')[0].Length -gt 15) { 15 } Else { $DomainName.Split('.')[0].Length }
-        Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "D:\NTDS" -DomainMode Win2012R2 -DomainName "$DomainName" -DomainNetbiosName "$($DomainName.Split('.')[0].Substring(0,$length))" -ForestMode Win2012R2 -InstallDns:$true -LogPath "D:\NTDS" -NoRebootOnCompletion:$false -SysvolPath "D:\SYSVOL" -Force:$true
-        Install-ADDSDomainController -CreateDnsDelegation:$false -Credential (New-Object System.Management.Automation.PSCredential("$Username", (ConvertTo-SecureString "$AdminPassword" -AsPlainText -Force))) -DatabasePath "D:\NTDS" -DomainName "$DomainName" -InstallDns:$true -LogPath "D:\NTDS" -NoGlobalCatalog:$false -SiteName "Default-First-Site-Name" -NoRebootOnCompletion:$false -SysvolPath "D:\SYSVOL" -Force:$true
+        Install-ADDSForest -DatabasePath "D:\NTDS" -DomainMode Win2012R2 -DomainName "$DomainName" -DomainNetbiosName "$($DomainName.Split('.')[0].Substring(0,$domainLength))" -ForestMode Win2012R2 -InstallDns -LogPath "D:\NTDS"  -SysvolPath "D:\SYSVOL" -Force
+        Install-ADDSDomainController -Credential (New-Object System.Management.Automation.PSCredential("$Username", (ConvertTo-SecureString "$AdminPassword" -AsPlainText -Force))) -DatabasePath "D:\NTDS" -DomainName "$DomainName" -InstallDns -LogPath "D:\NTDS" -NoGlobalCatalog:$false -SiteName "Default-First-Site-Name" -SysvolPath "D:\SYSVOL" -Force
 "@
 
     # Create AD Users and groups
